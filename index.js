7dcba0d8649387b0f6ee8af4c142fa42570e1610
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-15 03:23:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
 
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
 
-let shortGreeting = "Welcome, " + currentUser.charAtt(0)
+let shortGreeting = "Welcome, " + currentUser.charAt(0)
 
 
 
