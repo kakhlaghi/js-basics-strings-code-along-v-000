b9eb68eacb0a7d7af8c95f0201f3120f51bbaacc
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-15 03:17:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ let currentUser = "Something"
 
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
 
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK, "
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser 
 
