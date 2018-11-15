36ca8cec01a53e07788ecf596c806bc5865191db
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-15 03:19:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let currentUser = "Something"
 
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
 
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
 
 
 
